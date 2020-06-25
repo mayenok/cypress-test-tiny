6e35720ce01f9283f5c2b7646e19a9767020f064
anastasiia <anastasiia.fedorova@simbuka.com>
2020-06-25 12:06:49 +0200

    color input does not show palette spec

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('color-input', () => {
+    it('opens color palette', () => {
+        cy.visit('http://color-input.mayenok.com')
+            .get('input')
+            .click();
+        // color palette does not appear
+        // however it appears after manual click
+    })
 })
